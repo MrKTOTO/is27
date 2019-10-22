df5568be0eb8322b676d854d11d829e7f1bec4b5
Your Name <you@example.com>
2019-10-22 14:15:09 +0300

    123

---
+++ averyanova.cpp
@@ -0,0 +1,22 @@
+//
+// Created by averyanova27 on 10/22/19.
+//
+
+#include <iostream>
+
+using namespace std;
+
+void privet()
+{
+    printf("Привет");
+}
+void poka()
+{
+    printf("Пока");
+}
+int averyanova(int argc, char** argv)
+{
+    privet();
+    poka();
+    return 0;
+}
