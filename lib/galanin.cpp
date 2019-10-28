83807b691a6808b376bf7ba90a874778ebe5a258
Evgeny257 <galanin.zhenia@yandex.ru>
2019-10-28 18:35:11 +0300

    Create galanin.cpp

---
+++ lib/galanin.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+
+
+using namespace std;
+
+void privet()
+{
+	printf("Hello");
+}
+void poka()
+{
+	printf("Bye");
+}
+int galanin(int argc, char** argv)
+{
+	privet();
+	poka();
+	return 0;
+}
